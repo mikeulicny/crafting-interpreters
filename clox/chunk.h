96605e2433d826aa9495a70bbda11055a06c9ec5
Mike Ulicny <MichaelAUlicny@gmail.com>
2026-03-30 21:57:19 -0400

    Complete 14.5

--- clox/chunk.h
+++ clox/chunk.h
@@ -2,20 +2,24 @@
 #define CLOX_CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
 typedef struct {
-    int count;
-    int capacity;
-    uint8_t *code;
+    int        count;
+    int        capacity;
+    uint8_t    *code;
+    ValueArray constants;
 } Chunk;
 
 
 void init_chunk(Chunk *chunk);
 void free_chunk(Chunk *chunk);
 void write_chunk(Chunk *chunk, uint8_t byte);
+int add_constant(Chunk *chunk, Value value);
 
 #endif
--- clox/debug.c
+++ clox/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 void disassemble_chunk(Chunk *chunk, const char *name) {
     printf("== %s ==\n", name);
@@ -8,6 +9,14 @@ void disassemble_chunk(Chunk *chunk, const char *name) {
     for (int offset = 0; offset < chunk->count; ) {
         offset = disassemble_instruction(chunk, offset);
     }
+}
+
+static int constant_instruction(const char *name, Chunk *chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    print_value(chunk->constants.values[constant]);
+    printf("\n");
+    return offset + 2;
 }
 
 static int simple_instruction(const char *name, int offset) {
@@ -20,6 +29,8 @@ int disassemble_instruction(Chunk *chunk, int offset) {
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_CONSTANT:
+            return constant_instruction("OPCONSTANT", chunk, offset);
         case OP_RETURN:
             return simple_instruction("OP_RETURN", offset);
         default:
