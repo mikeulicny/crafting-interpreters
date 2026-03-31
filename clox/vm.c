30afb52445831ce1e35b1081b0052ced5910db47
Mike Ulicny <MichaelAUlicny@gmail.com>
2026-03-30 23:17:53 -0400

    Complete 15.3

--- clox/vm.c
+++ clox/vm.c
@@ -29,6 +29,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)     \
+    do {                  \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (0)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -48,6 +54,11 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN: {
                 print_value(pop());
                 printf("\n");
@@ -57,6 +68,7 @@ static InterpretResult run() {
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
