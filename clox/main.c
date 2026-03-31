231649913cef375c6ef95891820eb9b18c9833aa
Mike Ulicny <MichaelAUlicny@gmail.com>
2026-03-30 22:44:39 -0400

    Complete 15.1

--- clox/main.c
+++ clox/main.c
@@ -1,19 +1,22 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    init_VM();
+
     Chunk chunk;
     init_chunk(&chunk);
 
     int constant = add_constant(&chunk, 1.2);
     write_chunk(&chunk, OP_CONSTANT, 123);
     write_chunk(&chunk, constant, 123);
-
     write_chunk(&chunk, OP_RETURN, 123);
     
     disassemble_chunk(&chunk, "test chunk");
-
+    interpret(&chunk);
+    free_VM();
     free_chunk(&chunk);
     return 0;
 }
---
+++ clox/vm.h
@@ -0,0 +1,22 @@
+#ifndef CLOX_VM_H
+#define CLOX_VM_H
+
+#include "chunk.h"
+
+typedef struct {
+    Chunk   *chunk;
+    uint8_t *ip;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void init_VM();
+void free_VM();
+
+InterpretResult interpret(Chunk *chunk);
+
+#endif
