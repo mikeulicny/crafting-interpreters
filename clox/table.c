a3b809158cc00cb302015d5b119ea34c1a56cf8c
Mike Ulicny <MichaelAUlicny@gmail.com>
2026-04-03 13:49:15 -0400

    Complete 20.1, 20.2, 20.3, 20.4

---
+++ clox/table.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
+
+#define TABLE_MAX_LOAD 0.75
+
+void init_table(Table *table) {
+    table->count = 0;
+    table->capacity = 0;
+    table->entries = NULL;
+}
+
+void free_table(Table *table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    init_table(table);
+}
+
+static Entry *find_entry(Entry *entries, int capacity, ObjString *key) {
+    uint32_t index = key->hash % capacity;
+    Entry *tombstone = NULL;
+
+    for (;;) {
+        Entry *entry = &entries[index];
+        if (entry->key == NULL) {
+            if (IS_NIL(entry->value)) {
+                // Empty entry
+                return tombstone != NULL ? tombstone : entry;
+            } else {
+                // Found a tombstone
+                if (tombstone == NULL) tombstone = entry;
+            }
+        } else if (entry->key == key) {
+            // Found the key
+            return entry;
+        }
+
+        index = (index + 1) % capacity;
+    }
+}
+
+bool table_get(Table *table, ObjString *key, Value *value) {
+    if (table->count == 0) return false;
+
+    Entry *entry = find_entry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
+static void adjust_capacity(Table *table, int capacity) {
+    Entry *entries = ALLOCATE(Entry, capacity);
+    for (int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    table->count = 0; // Reset to 0 to ignore tombstone counts
+    for(int i = 0; i < table->capacity; i++) {
+        Entry *entry = &table->entries[i];
+        if (entry->key == NULL) continue;
+
+        Entry *dest = find_entry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+        table->count++; // Entry count incremented for non-tombstones only
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
+bool table_set(Table *table, ObjString *key, Value value) {
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjust_capacity(table, capacity);
+    }
+
+    Entry *entry = find_entry(table->entries, table->capacity, key);
+    bool is_new_key = entry->key == NULL;
+    if (is_new_key && IS_NIL(entry->value)) table->count++;
+
+    entry->key = key;
+    entry->value = value;
+    return is_new_key;
+}
+
+bool table_delete(Table *table, ObjString *key) {
+    if (table->count == 0) return false;
+
+    Entry *entry = find_entry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    // Place a tombstone in the entry
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+    return true;
+}
+
+void table_add_all(Table *from, Table *to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry *entry = &from->entries[i];
+        if (entry->key != NULL) {
+            table_set(to, entry->key, entry->value);
+        }
+    }
+}
