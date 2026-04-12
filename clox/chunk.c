61feb81b5873dac8817d89dc7d6c808d851bfc8d
Mike Ulicny <MichaelAUlicny@gmail.com>
2026-04-12 02:03:30 -0400

    Complete 26.7

--- clox/chunk.c
+++ clox/chunk.c
@@ -2,6 +2,7 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "vm.h"
 
 void init_chunk(Chunk *chunk) {
     chunk->count = 0;
@@ -31,6 +32,8 @@ void write_chunk(Chunk *chunk, uint8_t byte, int line) {
 }
 
 int add_constant(Chunk *chunk, Value value) {
+    push(value);
     write_value_array(&chunk->constants, value);
+    pop();
     return chunk->constants.count - 1;
 }
